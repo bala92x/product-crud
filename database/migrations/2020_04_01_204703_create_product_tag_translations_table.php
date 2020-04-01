d89ce97527930c759f84f666334d6aa85d39c207
Budavölgyi Bálint <budavolgyi.balint@gmail.com>
2020-04-01 23:22:56 +0200

    [FEATURE] Add table schemas and foreign key constraints

---
+++ database/migrations/2020_04_01_204703_create_product_tag_translations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTagTranslationsTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('product_tag_translations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('product_tag_translations');
+    }
+}
